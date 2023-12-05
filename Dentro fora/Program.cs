64122b7c2f6e51bd6777851a33f49ec8c51fd1a0
Tony <antony.branquinho@gmail.com>
2023-12-05 15:04:19 -0300

    facil

---
+++ Dentro fora/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dentro_fora {
+    class Program {
+        static void Main(string[] args) {
+
+            int n, i, x, dentro, fora;
+
+            Console.Write("Quantos numeros voce vai digitar? ");
+            n = int.Parse(Console.ReadLine());
+            
+            dentro = 0;
+            for (i = 0; i < n; i++) {
+                Console.Write("Digite um numero: ");
+                x = int.Parse(Console.ReadLine());
+                if (x >= 10 && x <= 20) {
+                    dentro = dentro + 1;
+                }
+            }
+
+            fora = n - dentro;
+            Console.WriteLine(dentro + " DENTRO.");
+            Console.WriteLine(fora + " FORA.");
+        }
+    }
+}
